45e8e96cd9be7bb81153352b4b158abce65da57f
jashimgit <jashim.ncn@gmail.com>
2021-04-24 04:53:56 +0600

    initial commit

---
+++ src/dummyData/dinner.js
@@ -0,0 +1,40 @@
+const dinner = [
+    {
+        title: 'Salmon with Grapefruit and Lentil Salad',
+        shortDescription: 'How we dream about our future',
+        price: 9.99,
+        imgUrl: 'https://i.ibb.co/fdpLSpR/dinner6.png',
+    },
+    {
+        title: 'Breakfast Sandwich',
+        shortDescription: 'How we dream about our future',
+        price: 9.99,
+        imgUrl: '',
+    },
+    {
+        title: 'Baked Chiken',
+        shortDescription: 'How we dream about our future',
+        price: 10.99,
+        imgUrl: 'https://i.ibb.co/wcprYLg/breakfast3.png',
+    },
+    {
+        title: 'Eggs Benedict',
+        shortDescription: 'How we dream about our future',
+        price: 8.99,
+        imgUrl: 'https://i.ibb.co/vPf0t5v/breakfast1.png',
+    },
+    {
+        title: 'Toast Croissant Fried egg',
+        shortDescription: 'How we dream about our future',
+        price: 19.99,
+        imgUrl: 'https://i.ibb.co/kDDGPqb/breakfast6.png',
+    },
+    {
+        title: 'Full Breakfast Fried Egg Toast Brunch',
+        shortDescription: 'How we dream about our future',
+        price: 3.99,
+        imgUrl: 'https://i.ibb.co/rxj4P1j/breakfast5.png',
+    },
+];
+
+export default dinner;
